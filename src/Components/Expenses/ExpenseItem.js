6ddd4c7bfd6ae3f92a5f3daa133c95c28d884a6b
EdlynD <edlyn.david19@gmail.com>
2022-08-05 11:46:53 +0800

    remove onlick

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const handleOnClick = () => {
+    console.log("Clicked");
+  };
+  return (
+    <Card className="expense-item-container">
+      <div className="date-container">
+        <ExpenseDate date={props.date} />
+      </div>
+      <div className="expense-item-description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item-price">${props.amount}</div>
+      <button>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
