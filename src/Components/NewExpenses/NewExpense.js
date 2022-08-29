c2b272777f9f3f96ac04303c7c7653637fa19d60
tredlyndavid <110870074+tredlyndavid@users.noreply.github.com>
2022-08-29 13:48:13 +0800

    hello

--- src/Components/NewExpenses/NewExpense.js
+++ src/Components/NewExpenses/NewExpense.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const isEditing = () => {
+    setIsFormVisible(true);
+  };
+  const isNotEditing = () => {
+    setIsFormVisible(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      {isFormVisible ? (
+        <ExpenseForm onClickNot={isNotEditing} />
+      ) : (
+        <button className="add-new-expense" onClick={isEditing}>
+          Add New Expenses
+        </button>
+      )}
     </div>
   );
 };
